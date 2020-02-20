711765d6c67789b78584a593f3140ee2a329fdc4
Tatiana Veratti <TatianaVeratti@gmail.com>
2020-02-20 01:59:38 -0600

    initial setup

---
+++ RustMeter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Oxide.Core;
+
+namespace Oxide.Plugins
+{
+  [Info("Rust Meter", "Cers", "0.0.1")]
+  [Description("Diminishing returns on actions after play time exceeds configurable limit.")]
+  public class RustMeter
+  {
+  }
+}
